9db851a76c973ab2c3b99e3f8768e65873436117
creamynald <creamynald@gmail.com>
2025-01-08 15:19:50 +0700

    add: controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,23 @@
 <?php
 
 use App\Http\Controllers\admin\dashboardController;
+use App\Http\Controllers\admin\faqController;
+use App\Http\Controllers\admin\informasiLayananController;
+use App\Http\Controllers\admin\profileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('theme.default.pages.landing.index');
 });
 
 // DAHSBOARD
 Route::prefix('admin')->group(function () {
     Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('layanan', [informasiLayananController::class, 'index']);  
     Route::get('layanan/{id_layanan}', [dashboardController::class, 'layanan'])->name('dashboard.detail_layanan');
+
+
+    Route::get('profile', [profileController::class, 'index']);
+    Route::get('faq', [faqController::class, 'index']);
 });
