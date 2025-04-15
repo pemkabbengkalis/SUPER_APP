3ee955544d66941d785552f4cbb80d4351481475
rere <creamynald@gmail.com>
2025-04-16 00:00:34 +0700

    (chg) update view profile

--- resources/views/theme/default/pages/dashboard/profile/index.blade.php
+++ resources/views/theme/default/pages/dashboard/profile/index.blade.php
@@ -2,186 +2,135 @@
 
 @section('dashboard-content')
 <div class="row">
+    <!-- Kolom My Profile -->
     <div class="col-xl-4">
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title mb-0">My Profile</h4>
-          <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
-        </div>
-        <div class="card-body">
-          <form>
-            <div class="row mb-2">
-              <div class="profile-title">
-                <div class="media">
-                    <img class="img-70 rounded-circle" alt="" src="../assets/images/user/7.jpg">
-                  <div class="media-body">
-                    <h5 class="mb-1">MARK JECNO</h5>
-                    <p>DESIGNER</p>
-                  </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">My Profile</h4>
+                <div class="card-options">
+                    <a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                    <a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a>
                 </div>
-              </div>
             </div>
-            <div class="mb-3">
-              <h6 class="form-label">Bio</h6>
-              <textarea class="form-control" rows="5">On the other hand, we denounce with righteous indignation</textarea>
+            <div class="card-body">
+                <form>
+                    <div class="row mb-2">
+                        <div class="profile-title">
+                            <div class="media">
+                                <img class="img-70 rounded-circle" alt="" src="{{ asset('theme/assets/images/avtar/default_profile_image.webp') }}">
+                              <div class="media-body">
+                                <h5 class="mb-1">{{ $name ?? 'Nama Pengguna' }}</h5>
+                                <p>{{ $jenis_kelamin ?? 'Jenis Kelamin' }}</p>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Email Address</label>
+                        <input class="form-control" placeholder="{{ $email ?? 'your-email@domain.com' }}" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">NIK</label>
+                        <input class="form-control" placeholder="{{ $nik ?? 'NIK tidak tersedia' }}" disabled>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-              <label class="form-label">Email-Address</label>
-              <input class="form-control" placeholder="your-email@domain.com">
-            </div>
-            <div class="mb-3">
-              <label class="form-label">Password</label>
-              <input class="form-control" type="password" value="password">
-            </div>
-            <div class="mb-3">
-              <label class="form-label">Website</label>
-              <input class="form-control" placeholder="http://Uplor .com">
-            </div>
-            <div class="form-footer">
-              <button class="btn btn-primary btn-block">Save</button>
-            </div>
-          </form>
         </div>
-      </div>
     </div>
+
+    <!-- Bagian Edit Profile -->
     <div class="col-xl-8">
-      <form class="card">
-        <div class="card-header">
-          <h4 class="card-title mb-0">Edit Profile</h4>
-          <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-5">
-              <div class="mb-3">
-                <label class="form-label">Company</label>
-                <input class="form-control" type="text" placeholder="Company">
-              </div>
+        <form class="card" action="{{ route('profile.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="card-header">
+                <h4 class="card-title mb-0">Edit Profile</h4>
+                <div class="card-options">
+                    <a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                    <a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a>
+                </div>
             </div>
-            <div class="col-sm-6 col-md-3">
-              <div class="mb-3">
-                <label class="form-label">Username</label>
-                <input class="form-control" type="text" placeholder="Username">
-              </div>
+            <div class="card-body">
+                <div class="row">
+                    <!-- Input NIK (Read-Only) -->
+                    <div class="col-md-5">
+                        <div class="mb-3">
+                            <label class="form-label">NIK</label>
+                            <input class="form-control" type="text" name="nik" value="{{ $nik ?? 'NIK tidak tersedia' }}" readonly>
+                        </div>
+                    </div>
+
+                    <!-- Input Email (Read-Only) -->
+                    <div class="col-sm-6 col-md-7">
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input class="form-control" type="email" name="email" value="{{ $email ?? 'Email tidak tersedia' }}" readonly>
+                        </div>
+                    </div>
+
+                    <!-- Input Jenis Kelamin -->
+                    <div class="col-sm-6 col-md-4">
+                        <div class="mb-3">
+                            <label class="form-label">Jenis Kelamin</label>
+                            <select class="form-control" name="jenis_kelamin" required>
+                                <option value="Laki-laki">Laki-laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- Input Tempat Lahir -->
+                    <div class="col-sm-6 col-md-4">
+                        <div class="mb-3">
+                            <label class="form-label">Tempat Lahir</label>
+                            <input class="form-control" type="text" name="tempat_lahir" placeholder="Masukkan Tempat Lahir" required>
+                        </div>
+                    </div>
+
+                    <!-- Input Tanggal Lahir -->
+                    <div class="col-sm-6 col-md-4">
+                        <div class="mb-3">
+                            <label class="form-label">Tanggal Lahir</label>
+                            <input class="form-control" type="date" name="tanggal_lahir" required>
+                        </div>
+                    </div>
+
+                    <!-- Input Alamat -->
+                    <div class="col-sm col-md-12">
+                        <div class="mb-3">
+                            <label class="form-label">Alamat</label>
+                            <textarea class="form-control" rows="4" name="alamat" placeholder="Masukkan Alamat"></textarea>
+                        </div>
+                    </div>
+
+                    <!-- Input Kelurahan -->
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label class="form-label">Kelurahan</label>
+                            <input class="form-control" type="text" name="kelurahan" placeholder="Masukkan Kelurahan" required>
+                        </div>
+                    </div>
+
+                    <!-- Input Kecamatan -->
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label class="form-label">Kecamatan</label>
+                            <input class="form-control" type="text" name="kecamatan" placeholder="Masukkan Kecamatan" required>
+                        </div>
+                    </div>
+
+                    <!-- Upload KTP -->
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label class="form-label">Upload KTP (Opsional)</label>
+                            <input class="form-control" type="file" name="ktp">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-6 col-md-4">
-              <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input class="form-control" type="email" placeholder="Email">
-              </div>
+            <div class="card-footer text-end">
+                <button class="btn btn-primary" type="submit">Update Profile</button>
             </div>
-            <div class="col-sm-6 col-md-6">
-              <div class="mb-3">
-                <label class="form-label">First Name</label>
-                <input class="form-control" type="text" placeholder="Company">
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-6">
-              <div class="mb-3">
-                <label class="form-label">Last Name</label>
-                <input class="form-control" type="text" placeholder="Last Name">
-              </div>
-            </div>
-            <div class="col-md-12">
-              <div class="mb-3">
-                <label class="form-label">Address</label>
-                <input class="form-control" type="text" placeholder="Home Address">
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-              <div class="mb-3">
-                <label class="form-label">City</label>
-                <input class="form-control" type="text" placeholder="City">
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-              <div class="mb-3">
-                <label class="form-label">Postal Code</label>
-                <input class="form-control" type="number" placeholder="ZIP Code">
-              </div>
-            </div>
-            <div class="col-md-5">
-              <div class="mb-3">
-                <label class="form-label">Country</label>
-                <select class="form-control btn-square">
-                  <option value="0">--Select--</option>
-                  <option value="1">Germany</option>
-                  <option value="2">Canada</option>
-                  <option value="3">Usa</option>
-                  <option value="4">Aus</option>
-                </select>
-              </div>
-            </div>
-            <div class="col-md-12">
-              <div>
-                <label class="form-label">About Me</label>
-                <textarea class="form-control" rows="4" placeholder="Enter About your description"></textarea>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card-footer text-end">
-          <button class="btn btn-primary" type="submit">Update Profile</button>
-        </div>
-      </form>
+        </form>
     </div>
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title mb-0">Add projects And Upload</h4>
-          <div class="card-options"><a class="card-options-collapse" href="#" data-bs-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a><a class="card-options-remove" href="#" data-bs-toggle="card-remove"><i class="fe fe-x"></i></a></div>
-        </div>
-        <div class="table-responsive add-project custom-scrollbar">
-          <table class="table card-table table-vcenter text-nowrap">
-            <thead>
-              <tr>
-                <th>Project Name</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th>Price</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><a class="text-inherit" href="#">Untrammelled prevents </a></td>
-                <td>28 May 2018</td>
-                <td><span class="status-icon bg-success"></span> Completed</td>
-                <td>$56,908</td>
-                <td class="text-end"><a class="icon" href="javascript:void(0)"></a><a class="btn btn-primary btn-sm" href="javascript:void(0)"><i class="fa fa-pencil"></i> Edit</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-transparent btn-sm" href="javascript:void(0)"><i class="fa fa-link"></i> Update</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-danger btn-sm" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a></td>
-              </tr>
-              <tr>
-                <td><a class="text-inherit" href="#">Untrammelled prevents</a></td>
-                <td>12 June 2018</td>
-                <td><span class="status-icon bg-danger"></span> On going</td>
-                <td>$45,087</td>
-                <td class="text-end"><a class="icon" href="javascript:void(0)"></a><a class="btn btn-primary btn-sm" href="javascript:void(0)"><i class="fa fa-pencil"></i> Edit</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-transparent btn-sm" href="javascript:void(0)"><i class="fa fa-link"></i> Update</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-danger btn-sm" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a></td>
-              </tr>
-              <tr>
-                <td><a class="text-inherit" href="#">Untrammelled prevents</a></td>
-                <td>12 July 2018</td>
-                <td><span class="status-icon bg-warning"></span> Pending</td>
-                <td>$60,123</td>
-                <td class="text-end"><a class="icon" href="javascript:void(0)"></a><a class="btn btn-primary btn-sm" href="javascript:void(0)"><i class="fa fa-pencil"></i> Edit</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-transparent btn-sm" href="javascript:void(0)"><i class="fa fa-link"></i> Update</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-danger btn-sm" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a></td>
-              </tr>
-              <tr>
-                <td><a class="text-inherit" href="#">Untrammelled prevents</a></td>
-                <td>14 June 2018</td>
-                <td><span class="status-icon bg-warning"></span> Pending</td>
-                <td>$70,435</td>
-                <td class="text-end"><a class="icon" href="javascript:void(0)"></a><a class="btn btn-primary btn-sm" href="javascript:void(0)"><i class="fa fa-pencil"></i> Edit</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-transparent btn-sm" href="javascript:void(0)"><i class="fa fa-link"></i> Update</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-danger btn-sm" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a></td>
-              </tr>
-              <tr>
-                <td><a class="text-inherit" href="#">Untrammelled prevents</a></td>
-                <td>25 June 2018</td>
-                <td><span class="status-icon bg-success"></span> Completed</td>
-                <td>$15,987</td>
-                <td class="text-end"><a class="icon" href="javascript:void(0)"></a><a class="btn btn-primary btn-sm" href="javascript:void(0)"><i class="fa fa-pencil"></i> Edit</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-transparent btn-sm" href="javascript:void(0)"><i class="fa fa-link"></i> Update</a><a class="icon" href="javascript:void(0)"></a><a class="btn btn-danger btn-sm" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
 @endsection
